5e7ae70a378ad1199eb28ae92ea56fc5b266d892
vankiaio <liyi@vankia.cn>
2018-12-12 13:02:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const { Api, JsonRpc, RpcError, JsSignatureProvider } = require('eosjs');
 const fetch = require('node-fetch');                            // node only; not needed in browsers
 const { TextDecoder, TextEncoder } = require('text-encoding');  // node, IE11 and IE Edge Browsers
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://vankiaio:vankia123!@221.122.119.226:27017/admin";
+const url = "";
 const XE_URL = 'http://www.xe.com/a/ratesprovider.php?_=';
 
 // 服务器端口
