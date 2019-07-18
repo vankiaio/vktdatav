ed455de463f153e5de2894de2f4e5d81dc7bf372
tokyoliyi <tokyoliyi@gmail.com>
2019-07-18 18:16:53 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ var config = {
   VKTAPI_URL: "http://221.122.119.226:8888",
   XE_URL: "http://www.xe.com/a/ratesprovider.php?_=",
   SCATTER_API: "https://api.get-scatter.com",
-  defaultPrivateKey: "5KPgsKGggSYKVtJqCACAgZxgjbQwzJsqCPuCN71cxTr52UzKMd5", // makeaccouts
-  candyPrivateKey: "5Jv8HzeBfFiygsmnsCkQW98tw7mygah3nBVTw636xJedLhYvSCa", // vktokendapps
+  defaultPrivateKey: "", // makeaccouts
+  candyPrivateKey: "", // vktokendapps
 
   // 其他配置项...
 };
