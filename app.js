4ff15450d0ca4587e5699557a63c40fa681bc6f9
tokyoliyi <tokyoliyi@139.com>
2020-09-01 12:42:45 +0800

    ensure get today reward

--- app.js
+++ app.js
@@ -2451,7 +2451,7 @@ app.use('/api_oc_pe_candy_system/:path_param1/:path_param2', defaultLimiter, asy
         });
       });
       if(reward_info.length > 0){
-        return reward_info;
+        return cb(reward_info);
       }
     
       }).then(async (reward_info) => {
